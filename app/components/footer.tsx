84da94e04d1dbfe2c6fe22a4f7ccc26cfde3ccb2
mmislam <mmislam272@gmail.com>
2025-10-06 22:52:20 +0600

    footer fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,9 +4,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col bg-[#261B02] text-white py-10 px-6 md:px-16">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 items-start">
-        <div className="flex flex-col items-end justify-center h-full">
+    <footer className="flex flex-row bg-[#261B02] text-white py-10 px-6 md:px-16">
+        <div className="flex flex-col items-center justify-center h-full px-30">
           <Image
             src="/logo.svg"
             alt="Ridero Logo"
@@ -23,26 +22,30 @@ const Footer = () => {
         RIDERO
       </span>
         </div>
+    <div className="flex flex-col ">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-30 items-start">
+        
         <div className="space-y-4">
-          <p className="text-sm text-gray-300 leading-relaxed">
+          <p className="text-sm text-white leading-relaxed">
             5123 Market St. #22B 
 Charlottesville, California 44635
           </p>
         </div>
 
-        <div>
+        <div >
 
-          <ul className="space-y-2 text-gray-300 text-sm">
-            <li><Link href="/about" className="hover:text-[#EFB639]">About</Link></li>
-            <li><Link href="/features" className="hover:text-[#EFB639]">Features</Link></li>
-            <li><Link href="/pricing" className="hover:text-[#EFB639]">Pricing</Link></li>
-            <li><Link href="/careers" className="hover:text-[#EFB639]">Careers</Link></li>
+          <ul className="space-y-2 text-white text-sm">
+            <li><Link href="/Home" className="hover:text-[#EFB639]">home</Link></li>
+            <li><Link href="/How It Works" className="hover:text-[#EFB639]">How It Works</Link></li>
+            <li><Link href="/Features" className="hover:text-[#EFB639]">Features</Link></li>
+            <li><Link href="/Contact" className="hover:text-[#EFB639]">Contact</Link></li>
+            <li><Link href="/Testimonial" className="hover:text-[#EFB639]">Testimonial</Link></li>
           </ul>
         </div>
 
         <div>
           
-          <ul className="space-y-2 text-gray-300 text-sm">
+          <ul className="space-y-2 text-white text-sm">
             <li><Link href="/Facebook" className="hover:text-[#EFB639]">Facebook</Link></li>
             <li><Link href="/Twitter" className="hover:text-[#EFB639]">Twitter</Link></li>
             <li><Link href="/Linkedin" className="hover:text-[#EFB639]">Linkedin</Link></li>
@@ -51,16 +54,17 @@ Charlottesville, California 44635
         </div>
 
       </div>
-      <div className="flex flex-row items-center " >
+      <div className="flex flex-row items-center gap-30 border-red mt-10" >
         <div>
-            <p>
+            <p className="space-y-2 text-white text-sm" >
                 (434) 546-4356
             </p>
-            <p>
+            <p className="text-white text-sm">
                 contact@lift.agencyr.com
             </p>
         </div>
-        <p>© 2020 Ridero. All rights reserved.</p>
+        <p className="text-white text-sm">© 2020 Lift media. All rights reserved.</p>
+      </div>
       </div>
     </footer>
   );
