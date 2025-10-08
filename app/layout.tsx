808b3b74403c4ad75d95a852619a190ad99c26d7
mmislam <mmislam272@gmail.com>
2025-10-08 12:57:47 +0600

    tsx fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar.tsx";
+import Navbar from "./components/navbar";
 import ReduxProvider from "./providers/ReduxProvider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
