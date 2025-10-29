e20fa67964b0fb7de783ec96594e7fe461420bcf
mm <mmislam272@gmail.com>
2025-10-30 05:09:25 +0600

    stable footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,66 +5,97 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full flex flex-row bg-[#261B02] justify-center text-white py-10 px-6 md:px-16">
-        <div className="flex flex-col items-center justify-center h-full px-40">
-          <Image
-            src="/logo.svg"
-            alt="Ridero Logo"
-            width={44}
-            height={44}
-            className="object-contain"
-          />
-          <span
-        className=" font-semibold text-center"
-        style={{
-          fontSize: 16,
-        }}
-      >
-        RIDERO
-      </span>
-        </div>
-    <div className="flex flex-col ">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-30 items-start">
-        
-        <div className="space-y-4">
+      <div className="flex flex-col items-center justify-center h-full px-4 md:px-40">
+        <Image
+          src="/logo.svg"
+          alt="Ridero Logo"
+          width={44}
+          height={44}
+          className="object-contain"
+        />
+        <span
+          className=" font-semibold text-center"
+          style={{
+            fontSize: 16,
+          }}
+        >
+          RIDERO
+        </span>
+      </div>
+
+      <div className="flex flex-col md:flex-row ">
+        <div className="space-y-4 mb-6 ">
           <p className="text-sm text-white leading-relaxed">
-            5123 Market St. #22B 
-Charlottesville, California 44635
+            5123 Market St. #22B Charlottesville, California 44635
           </p>
         </div>
 
-        <div >
+        <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-30 md:ml-30 items-start">
+          <div>
+            <ul className="space-y-2 text-white text-sm">
+              <li>
+                <Link href="/Home" className="hover:text-[#EFB639]">
+                  home
+                </Link>
+              </li>
+              <li>
+                <Link href="/How It Works" className="hover:text-[#EFB639]">
+                  How It Works
+                </Link>
+              </li>
+              <li>
+                <Link href="/Features" className="hover:text-[#EFB639]">
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link href="/Contact" className="hover:text-[#EFB639]">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link href="/Testimonial" className="hover:text-[#EFB639]">
+                  Testimonial
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-          <ul className="space-y-2 text-white text-sm">
-            <li><Link href="/Home" className="hover:text-[#EFB639]">home</Link></li>
-            <li><Link href="/How It Works" className="hover:text-[#EFB639]">How It Works</Link></li>
-            <li><Link href="/Features" className="hover:text-[#EFB639]">Features</Link></li>
-            <li><Link href="/Contact" className="hover:text-[#EFB639]">Contact</Link></li>
-            <li><Link href="/Testimonial" className="hover:text-[#EFB639]">Testimonial</Link></li>
-          </ul>
+          <div>
+            <ul className="space-y-2 text-white text-sm">
+              <li>
+                <Link href="/Facebook" className="hover:text-[#EFB639]">
+                  Facebook
+                </Link>
+              </li>
+              <li>
+                <Link href="/Twitter" className="hover:text-[#EFB639]">
+                  Twitter
+                </Link>
+              </li>
+              <li>
+                <Link href="/Linkedin" className="hover:text-[#EFB639]">
+                  Linkedin
+                </Link>
+              </li>
+              <li>
+                <Link href="/Instagram" className="hover:text-[#EFB639]">
+                  Instagram
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div>
-          
-          <ul className="space-y-2 text-white text-sm">
-            <li><Link href="/Facebook" className="hover:text-[#EFB639]">Facebook</Link></li>
-            <li><Link href="/Twitter" className="hover:text-[#EFB639]">Twitter</Link></li>
-            <li><Link href="/Linkedin" className="hover:text-[#EFB639]">Linkedin</Link></li>
-            <li><Link href="/Instagram" className="hover:text-[#EFB639]">Instagram</Link></li>
-          </ul>
+        <div className="flex flex-col md:flex-row items-start gap-30 border-red mt-10">
+          <div>
+            <p className="space-y-2 text-white text-sm">(434) 546-4356</p>
+            <p className="text-white text-sm">contact@lift.agencyr.com</p>
+          </div>
+          <p className="text-white text-sm">
+            © 2020 Lift media. All rights reserved.
+          </p>
         </div>
-
-      </div>
-      <div className="flex flex-row items-center gap-30 border-red mt-10" >
-        <div>
-            <p className="space-y-2 text-white text-sm" >
-                (434) 546-4356
-            </p>
-            <p className="text-white text-sm">
-                contact@lift.agencyr.com
-            </p>
-        </div>
-        <p className="text-white text-sm">© 2020 Lift media. All rights reserved.</p>
-      </div>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import ReduxProvider from "./providers/ReduxProvider";
-import Footer from "./components/footer";
+import Footer from "./components/footer";   
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -17,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <ReduxProvider>
-        <body className={`${inter.variable} antialiased flex flex-col items-center w-full overflow-x-hidden`}>
+        <body className={`${inter.variable} antialiased flex flex-col items-center w-full max-w-full overflow-x-hidden`}>
           <header className=" w-full">
             <Navbar></Navbar>
           </header>
