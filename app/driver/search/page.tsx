39f65f36e205ef3618f9d84598ac493cac3469a0
mm <mmislam272@gmail.com>
2025-11-12 05:36:33 +0600

    driver ongoing

--- app/driver/search/page.tsx
+++ app/driver/search/page.tsx
@@ -3,10 +3,14 @@
 import React from "react";
 import Image from "next/image";
 import { useDeviceType } from "@/app/hooks/useDeviceType";
+import { useAppDispatch,useAppSelector } from "@/app/store/hooks";
 
 
 const page = () => {
     const isMobile=useDeviceType()
+    const driver=useAppSelector((store)=>store.driver)
+    const dispatch= useAppDispatch()
+
     return (
         <div className="w-full flex flex-col items-center justify-start">
             <div className="w-full flex flex-row items-center justify-start p-2">
@@ -14,14 +18,44 @@ const page = () => {
                     Welcome Back, Rahim
                 </h2>
             </div>
-            <div className="w-full flex flex-col md:flex-row gap-4 items-center justify-between">
-                <div className="w-full flex md:flex-1 flex-row items-center justify-center rounded-xl shadow-lg p-4 bg-white">
-                    <div className="flex items-center justify-center bg-gray-300 h-12 w-12 rounded-lg"></div>
-                    <div className="flex flex-1 flex-col items-start justify-between h-18 bg-green-400 px-3"></div>
+            <div className="w-full flex flex-col md:flex-row gap-6 items-center justify-between">
+                <div className="w-full flex md:flex-1 flex-row items-center justify-center rounded-xl shadow-lg p-4 px-8 gap-4 bg-white">
+                    <div className="flex items-center justify-center bg-gray-300 h-15 w-15 rounded-lg"></div>
+                    <div className="flex flex-1 flex-col items-start justify-between h-22 px-3">
+                        <p className="text-2xl">
+                            Total Earnings
+                        </p>
+                        <h1 className="text-5xl font-semibold">
+                            $212
+                        </h1>
+                    </div>
                 </div>
-                <div className="w-full md:flex-1 flex-row items-center justify-center rounded-xl shadow-lg p-4 bg-white"></div>
-                <div className="w-full md:flex-1 flex-row items-center justify-center rounded-xl shadow-lg p-4 bg-white"></div>
+                <div className="w-full flex md:flex-1 flex-row items-center justify-center rounded-xl shadow-lg p-4 px-8 gap-4 bg-white">
+                    <div className="flex items-center justify-center bg-gray-300 h-15 w-15 rounded-lg"></div>
+                    <div className="flex flex-1 flex-col items-start justify-between h-22 px-3">
+                        <p className="text-2xl">
+                            Pending Deliveries
+                        </p>
+                        <h1 className="text-5xl font-semibold">
+                            $212
+                        </h1>
+                    </div>
+                </div>
+                <div className="w-full flex md:flex-1 flex-row items-center justify-center rounded-xl shadow-lg p-4 px-8 gap-4 bg-white">
+                    <div className="flex items-center justify-center bg-gray-300 h-15 w-15 rounded-lg"></div>
+                    <div className="flex flex-1 flex-col items-start justify-between h-22 px-3">
+                        <p className="text-2xl">
+                            Completed Deliveries
+                        </p>
+                        <h1 className="text-5xl font-semibold">
+                            $212
+                        </h1>
+                    </div>
+                </div>
             </div>
+
+            { /*Actions Section */ }
+
             <div className="w-full flex flex-row items-center justify-start p-2">
                 <h2 className="text-xl md:text-2xl text-black font-semibold">Actions</h2>
             </div>
@@ -35,10 +69,14 @@ const page = () => {
                     Wait for delivery request
                 </h2>
             </div>
+
+            { /*Searching Section */}
+
+
             <div className="w-full flex flex-col gap-4 items-center justify-center bg-white rounded-xl mb-6 py-10">
                 <div className="w-full flex flex-row gap-2 items-center justify-center p-4">
                     <h2 className="flex flex-row text-xl md:text-3xl text-black font-semibold">
-                        We’re
+                        We're
                     </h2>
                     <h2 className="text-xl md:text-3xl text-purple-600 font-semibold">
                         Searching
